8a96b5cc4003dd63a283bbb55909e0f56f670205
Marcos Santana <marcos.vssjr@outlook.com>
2025-03-18 15:48:16 -0300

    projeto ja funcional

--- aluramidi-curso-arquivos-iniciais/main.js
+++ aluramidi-curso-arquivos-iniciais/main.js
@@ -1,13 +1,29 @@
-function tocaSomPom(){
-    document.querySelector('#som_tecla_pom').play();
-    
+function tocaSom(idElementoAudio){
+    document.querySelector(idElementoAudio).play();
 }
 
-document.querySelector('.tecla_pom').onclick=tocaSomPom; // caso estivse onclick=tocaSomPom(), 
+// document.querySelector('.tecla_pom').onclick=tocaSomPom; // caso estivse onclick=tocaSomPom(), 
 // a função ja iria iniciar sozinha, pois ela não estaria guardando a função de tocar som no onclick
 // ela ja vai estar iniciando assim que js ler as linhas 
 
+const listaDeTeclas = document.querySelectorAll('.tecla');
 
-document.querySelectorAll('.tecla');
 
-listaDeTeclas
+for( let contador=0; contador < listaDeTeclas.length;  contador++){
+   
+    const tecla = listaDeTeclas[contador]; 
+    // aqui percorremos os botões 
+
+    const instrumento = tecla.classList[1];
+     // aqui percorremos a segunda classe que pertence cada botão
+
+    const idAudio= `#som_${instrumento}`;
+    // console.log(idAudio);
+    //aqui temos a virada de chave, cada id do arquivo de audio se chama
+    // #som_tecla_pom e assim por diante,  ao colorcamos #som_${intrumento}, tornamos esse elemento variavel
+    // assim, cada vez que ativamos um botão, o som vai mudando e não ficando o mesmo  
+    tecla.onclick=function(){
+        tocaSom(idAudio);
+    }
+  //  console.log(contador);
+}
